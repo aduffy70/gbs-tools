a8f6a93dbead6401a2735203ad33decae16fe01f
aduffy70 <aduffy70@gmail.com>
2014-09-06 14:26:47 -0600

    Added checkfastqformat.py to look for formating problems in fastq files

---
+++ checkfastqformat.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+# checkfastqformat.py
+# Checks fastq files looking for extra blank lines, missing lines, etc.
+# It expects to find in order (and complains if it doesn't find):
+#   a header line starting with @HISEQ, 
+#   a data line with N, A, G, T, or C as the first character,
+#   a marker line with ONLY +, and
+#   a quality line that matches the length of dataline 
+# If there is a blank line at the end of the file it will complain that it is 
+# expecting a header line. Don't ignore this! A blank line at the end causes 
+# problems for some tools (I'm looking at YOU cutadapt!).
+# When it finds a problem it reports the type, the line number, and prints the line.
+# Usage: checkfastqformat.py fastqfiletobechecked.fastq
+import sys
+
+with open(sys.argv[1]) as file: # open the fastq file
+    line_count = 0
+    next_line = "header"
+    for line in file:
+        if line_count % 4 == 0:
+            #should be a header
+            if line[0:6] != "@HISEQ":
+                print "Expected header", line_count, line
+        elif line_count % 4 == 1:
+            #should be a dataline
+            if line[0] not in ["N", "G", "A","T", "C"]:
+                print "Expected dataline", line_count, line
+            else:
+                dataline_length = len(line)
+        elif line_count % 4 == 2:
+            #should be a "+"
+            if line != "+\n":
+                print "Expected +", line_count, line
+        elif line_count % 4 == 3:
+            #should be quality line
+            if line[0:6] == "@HISEQ" or line == "+\n":
+                print "Expected qualityline", line_count, line
+            elif dataline_length != len(line):
+                print "Quality line length doesn't match data line", line_count, line
+        line_count += 1
