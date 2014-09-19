9359c8fd2145daa92f53a837883dd83d9a619dae
aduffy70 <aduffy70@gmail.com>
2014-09-19 15:10:49 -0600

    Updated findunique12mers to work on other fastq files.

--- findunique12mers.py
+++ findunique12mers.py
@@ -5,7 +5,9 @@
 # including those with Ns?  
 # Actually an N can be handled just like any other mismatched base so
 # lets do this again with them included.
-# usage: ./findunique12mers.py fastqfile.fastq
+# You need to specify the starting string that indicates a header line
+# and the length of the longest barcode/stickyend combination.
+# usage: ./findunique12mers.py fastqfile.fastq headerstartstring maxbarcodelength
 
 import sys
 
@@ -14,11 +16,14 @@ sequence_count = 0
 n_count = 0
 clean_count = 0
 unique_starts = {}
+header_start = sys.argv[2]
+header_start_length = len(header_start)
+max_barcode_length = int(sys.argv[3])
 
 with open(sys.argv[1]) as file:
     for line in file:
         if header_line == 1: #This is the data line
-            target = line[0:12]
+            target = line[0:max_barcode_length]
 # Use this section to exclude 12mers containing Ns            
 #            if "N" in target:
 #                n_count += 1
@@ -39,7 +44,7 @@ with open(sys.argv[1]) as file:
 
             sequence_count += 1
             header_line = 0
-        elif line[0:6] == "@HISEQ": #This is the header, the dataline is next
+        elif line[0:header_start_length] == header_start: #This is the header, the dataline is next
             header_line = 1
 print "# Total reads:", sequence_count
 #print "# Reads with N's:", n_count
