e5ebb5a863eac9f24cba5b890843e0b8468e80f6
aduffy70 <aduffy70@gmail.com>
2014-09-19 11:36:03 -0600

    Updated checkfastqformat.py to allow it to be used on fastq files other than my Tint dataset.

--- checkfastqformat.py
+++ checkfastqformat.py
@@ -3,7 +3,7 @@
 # checkfastqformat.py
 # Checks fastq files looking for extra blank lines, missing lines, etc.
 # It expects to find in order (and complains if it doesn't find):
-#   a header line starting with @HISEQ, 
+#   a header line starting with a specified string (@HISEQ for my Tint data), 
 #   a data line with N, A, G, T, or C as the first character,
 #   a marker line with ONLY +, and
 #   a quality line that matches the length of dataline 
@@ -11,16 +11,18 @@
 # expecting a header line. Don't ignore this! A blank line at the end causes 
 # problems for some tools (I'm looking at YOU cutadapt!).
 # When it finds a problem it reports the type, the line number, and prints the line.
-# Usage: checkfastqformat.py fastqfiletobechecked.fastq
+# Usage: checkfastqformat.py fastqfiletobechecked.fastq expectedheaderstartstring
 import sys
 
 with open(sys.argv[1]) as file: # open the fastq file
     line_count = 0
+    header_start = sys.argv[2]
+    header_start_length = len(header_start)
     next_line = "header"
     for line in file:
         if line_count % 4 == 0:
             #should be a header
-            if line[0:6] != "@HISEQ":
+            if line[0:header_start_length] != header_start:
                 print "Expected header", line_count, line
         elif line_count % 4 == 1:
             #should be a dataline
@@ -34,8 +36,12 @@ with open(sys.argv[1]) as file: # open the fastq file
                 print "Expected +", line_count, line
         elif line_count % 4 == 3:
             #should be quality line
-            if line[0:6] == "@HISEQ" or line == "+\n":
+            if line[0:6] == header_start or line == "+\n":
                 print "Expected qualityline", line_count, line
             elif dataline_length != len(line):
                 print "Quality line length doesn't match data line", line_count, line
         line_count += 1
+    if line_count % 4 == 0:
+        print "Ended with a complete record"
+    else:
+        print "Ended with incomplete record"
