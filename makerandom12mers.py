352da42ef04a816a523ee88169b2edfe8ca1ba8a
aduffy70 <aduffy70@gmail.com>
2014-12-03 09:06:40 -0700

    makerandom12mers.py - Remove the hardcoded 12bp sequence length and add the length to the command line options. (I know, that means this no longer makes random "12mers", but makerandomwhatevermers.py is a dumb name for a script.)

--- makerandom12mers.py
+++ makerandom12mers.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python
 
 # makerandom12mers.py
-# Generates a fastq file with a specified number of random 12 base sequences.
+# Generates a fastq file with a specified number of random sequences of a
+# specified length.
 # Used this to increase my confidence that the results of my barcode matching
 # process are not just what might be expected by chance in a large dataset.
-# usage: ./makerandom12mers.py 50000000
+# usage: ./makerandom12mers.py number_of_sequences length_of_sequences
 
 import random
 import sys
@@ -12,7 +13,7 @@ import sys
 bases = ["G", "A", "T", "C"]
 for sequence in range(0, int(sys.argv[1])):
     new_sequence = ""
-    for base in range(0, 12):
+    for base in range(0, int(sys.argv[2])):
         new_sequence += random.choice(bases)
     print "@HISEQ-Dummyheader"
     print new_sequence
