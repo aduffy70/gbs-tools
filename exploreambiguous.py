19acabc0a6a4e541145404f98265f141833d4049
aduffy70 <aduffy70@gmail.com>
2014-12-10 11:22:30 -0700

    Updated exploreambiguous.py to work for any length of barcodes and for any wobble bases in the cutsite.

--- exploreambiguous.py
+++ exploreambiguous.py
@@ -1,74 +1,78 @@
 #! /usr/bin/python
 
 # exploreambiguous.py
-# When I run sortbarcodes.py I have 33million reads with
-# a best distance of 1 or 2 that are equally distant to
-# two or more barcodes so they can't be assigned. This script
-# will capture each of those ambiguous 12mers and list which
-# barcodes are equally distant so I can see if there is any
-# way to salvage those reads. Based on sortbarcodes.py but
-# printing just a list of the ambiguous 12mers and their
-# closest barcodes. 
-
-# Usage: exploreambiguous.py barcodefile.txt Twelvemerfile.txt
+# When I run sortbarcodes.py I have reads with low distances
+# that are equally distant to two or more barcodes so they
+# can't be assigned. This script lists those ambiguous
+# 12mers and the barcodes that are equally distant. There
+# was a question about how we could have reads with a best
+# match distance of 2 that could still not be assigned 
+# unambiguously if all our barcodes are 4 bases different
+# from each other. This script demonstrates how.
+# For example, these two barcodes are 4 bases apart but 
+# each is only 2 bases different from the example read:
+# CTCGTACCAACAATTC - example read 
+# ATCGGACCAACAATTC - barcode 1
+# CTGATACCAACAATTC - barcode 2
+# This version works for reads of any length and can handle
+# wobble bases in the cutsite, but does not allow for indels
+# when calculating distances between reads and barcodes.
 
 import sys
 import operator # for sorting dictionaries
 
 barcodes = {}
-distance_counts = [0,0,0,0,0,0,0,0,0,0,0,0,0] # Store the # of unique barcodes with each distance from the best match
-barcode_assignments = {}
-unambiguous = 0 # count the unambiguously assigned 12mers
-
-with open(sys.argv[1]) as file: # open the barcode file and make a dictionary
+min_barcode_length = 100 # Set it bigger than the biggest we expect for now
+max_barcode_length = 0 # Set it smaller than the smallest we expect for now
+# Make a dictionary of IUB nucleotide codes so we can handle ANY wobble bases
+nucleotides = {"A": "A", "T": "T", "C": "C", "G": "G",
+               "R": "GA",
+               "K": "GT",
+               "S": "GC",
+               "W": "AT",
+               "M": "AC",
+               "Y": "TC",
+               "D": "GAT",
+               "V": "GAC",
+               "B": "GTC",
+               "H": "ATC",
+               "N": "GATC"}
+with open(sys.argv[1]) as file: # Open the barcode file to make a dictionary and find the range of barcode lengths
     for line in file:
         if line[0] != "#": # Ignore lines starting with a #
-            split_line = line.split()
-            barcodes[split_line[1]] = split_line[0] # key=barcode, value=sample name
-with open(sys.argv[2]) as file: # open the 12mer file
+            split_line = line.replace(",", " ").split() # allows for comma or space delimited barcode files
+            new_barcode = split_line[1].upper() # allows for upper or lowercase barcodes
+            barcode_length = len(new_barcode)
+            if barcode_length < min_barcode_length:
+                min_barcode_length = barcode_length
+            elif barcode_length > max_barcode_length:
+                max_barcode_length = barcode_length
+            barcodes[new_barcode] = split_line[0] # key=barcode sequence, value=barcode name or sample name
+# Now we know the range of barcode lengths and can set up the distance lists at the correct length
+with open(sys.argv[2]) as file: # Open the 12mer file
     for line in file:
-        if line[0] != "#":  # ignore lines starting with a #
+        ambiguous_list = []
+        if line[0] != "#":  # Ignore lines starting with a #
             best_match = ""
-            best_distance = 13 # Default value worse than the worst possible match
+            best_distance = max_barcode_length + 1 # Start with a value worse than the worst possible match
             split_line = line.split()
             read_count = split_line[1]
             twelvemer = split_line[0]
-            elevenmer = split_line[0][0:-1]
-            tenmer = split_line[0][0:-2]
-            ninemer = split_line[0][0:-3]
-            eightmer = split_line[0][0:-4]
-            if barcodes.has_key(twelvemer): # exact match
-                unambiguous += 1
-            elif barcodes.has_key(elevenmer): # exact match
-                unambiguous += 1
-            elif barcodes.has_key(tenmer): # exact match
-                unambiguous += 1
-            elif barcodes.has_key(ninemer): # exact match
-                unambiguous += 1
-            elif barcodes.has_key(eightmer): # exact match
-                unambiguous += 1
-            else: # try to correct the barcode
-                ambiguous_list = []
-                for barcode in barcodes:
-                    distance = 0
-                    length = len(barcode)
-                    wobble_site = length - 3 # we will handle the W in the enzyme sticky end differently
-                    for x in range(0, length): # calculate the distance for this barcode
-                        if x != wobble_site:
-                            if twelvemer[x] != barcode[x]:
-                                distance += 1
-                        else:
-                            if twelvemer[x] != "A" and twelvemer[x] != "T":
-                                distance += 1
-                    if distance < best_distance: # Best match yet
-                        best_distance = distance
-                        best_match = barcode
-                        ambiguous_list = [barcode]
-                    elif distance == best_distance: # Can't choose between 2 best matches
-                        best_match = "AMBIGUOUS"
-                        ambiguous_list.append(barcode)
-                if best_match == "AMBIGUOUS" and best_distance < 3:
-                    print twelvemer, best_distance,
-                    for barcode in ambiguous_list:
-                        print barcode,
-                    print ""
+            for barcode in barcodes:
+                distance = 0
+                length = len(barcode)
+                for x in range(0, length): # Calculate the distance for this barcode
+                    if twelvemer[x] not in nucleotides[barcode[x]]: #this accounts for wobble nucleotides in the cutsite
+                        distance += 1
+                if distance < best_distance: # Best match yet, let's remember this one
+                    best_distance = distance
+                    best_match = barcode
+                    ambiguous_list = [barcode]
+                elif distance == best_distance: # Can't choose between 2 best matches
+                    best_match = "AMBIGUOUS"
+                    ambiguous_list.append(barcode)
+            if best_match == "AMBIGUOUS" and best_distance < 5:
+                print twelvemer, best_distance,
+                for barcode in ambiguous_list:
+                    print barcode,
+                print ""
