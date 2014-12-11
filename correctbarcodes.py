8f23870dd335cc4aa2b9769c8b2c9b38f8061557
aduffy70 <aduffy70@gmail.com>
2014-12-11 14:02:48 -0700

    Updated correctbarcodes.py to work with any length of barcodes and made the maximum acceptable distance configurable.

--- correctbarcodes.py
+++ correctbarcodes.py
@@ -7,8 +7,12 @@
 # Write those fastq reads to a new file (with the barcodes 
 # corrected but still in place for now).
 # Write all other fastq reads to a separate files so they can
-# be explored further (ambiguous reads w/distance <3 and reads with distance >=3) 
-# Usage: correctbarcodes.py sortbarcodesOUTfile.txt fastqdata.fastq correctedreadfile.fastq ambiguousreadfile.fastq distantreadfile.fastq 
+# be explored further (keeping the ambiguously assigned reads
+# separate from the reads with distance > minimum_acceptable) 
+# You need to specify the starting string that indicates a
+# header line, the maximum acceptable distance, and the files
+# where the various categories of reads should be stored.
+# Usage: correctbarcodes.py sortbarcodesOUTfile.txt fastqdata.fastq headerstartstring maxacceptabledistance correctedreadfile.fastq ambiguousreadfile.fastq distantreadfile.fastq 
 
 import sys
 import operator # for sorting dictionaries
@@ -17,15 +21,21 @@ barcode_assignments = {} # A lookup dictionary with unique 12mers as key and cor
 read_counts = {"CORRECTED": 0, "AMBIGUOUS": 0, "DISTANT": 0}
 total_read_count = 0
 output_files = {} # Store the 3 output files (saves on if statements later)
-output_files["CORRECTED"] = open(sys.argv[3], "w")
-output_files["AMBIGUOUS"] = open(sys.argv[4], "w")
-output_files["DISTANT"] = open(sys.argv[5], "w")
+output_files["CORRECTED"] = open(sys.argv[5], "w")
+output_files["AMBIGUOUS"] = open(sys.argv[6], "w")
+output_files["DISTANT"] = open(sys.argv[7], "w")
+header_start = sys.argv[3]
+header_start_length = len(header_start)
+max_distance = int(sys.argv[4])
+twelvemer_length = 0 # We will determine the length of the "12mers" so this can work for any length barcodes
+
 
 with open(sys.argv[1]) as file: # open the sortbarcodesOUT file and make a dictionary
     for line in file:
         if line[0] != "#": # Ignore lines starting with a #
             split_line = line.split()
             barcode_assignments[split_line[0]] = [split_line[1], split_line[2]] # key=12mer, value=[barcode, distance]
+    twelvemer_length = len(split_line[0]) # They are all the same length as the last one
 with open(sys.argv[2]) as file: # open the fastqdata file
     fastq_line = 0
     header = ""
@@ -35,24 +45,24 @@ with open(sys.argv[2]) as file: # open the fastqdata file
     disposition = "" 
     for line in file:
         if fastq_line == 1: # This is the dataline
-            twelvemer = line[0:12]
-            distance = int(barcode_assignments[twelvemer][1])
-            if distance > 2: # This is too distant to assign to a barcode
-                disposition = "DISTANT"
+            twelvemer = line[0:twelvemer_length]
+            corrected_barcode = barcode_assignments[twelvemer][0]
+            if corrected_barcode == "AMBIGUOUS": # This couldn't be assigned unambiguously
+                disposition = "AMBIGUOUS"
                 data = line # Keep the read as-is
             else:
-                corrected_barcode = barcode_assignments[twelvemer][0]
-                if corrected_barcode == "AMBIGUOUS": # This couldn't be assigned unambiguously
-                    disposition = "AMBIGUOUS"
+                distance = int(barcode_assignments[twelvemer][1])
+                if distance > max_distance: # This is too distant to assign to a barcode
+                    disposition = "DISTANT"
                     data = line # Keep the read as-is
-                else: # distance is <3 and unambiguously assigned a barcode
+                else: # distance is <= max_distance and unambiguously assigned a barcode
                     disposition = "CORRECTED"
                     data = corrected_barcode + line[len(corrected_barcode):] # replace the wrong barcode
             fastq_line = 2
         elif fastq_line == 3: # This is the qualityline
             quality = line
             fastq_line = 0
-        elif line[0:6] == "@HISEQ": # This is the header, the dataline is next
+        elif line[0:header_start_length] == header_start: # This is the header, the dataline is next
             header = line
             fastq_line = 1
         elif line[0] == "+": # The quality line is next
@@ -62,11 +72,10 @@ with open(sys.argv[2]) as file: # open the fastqdata file
             output_files[disposition].writelines([header, data, marker, quality])
             read_counts[disposition] += 1                    
             total_read_count += 1
-            if total_read_count % 100000 == 0:
+            if total_read_count % 100000 == 0: # Give some output so we can see how things are going
                 print "Reads:", total_read_count, "Corrected:", read_counts["CORRECTED"], \
                     "Ambiguous:", read_counts["AMBIGUOUS"], "Distant:", read_counts["DISTANT"]
     print "Reads:", total_read_count, "Corrected:", read_counts["CORRECTED"], \
                     "Ambiguous:", read_counts["AMBIGUOUS"], "Distant:", read_counts["DISTANT"]
 for key in output_files:
     output_files[key].close()
-
